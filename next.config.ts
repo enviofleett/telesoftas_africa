0d99d0dbf7fdbbb0da445e4900d06aecdc619153
MarcorpAI <markkaave@gmail.com>
2026-02-15 16:18:19 +0100

    mobile view changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/#contact',
+        permanent: true,
+      },
+    ];
+  },
 };
+
 
 export default nextConfig;
